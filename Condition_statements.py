c8608d43730cc5757a0fd87471c882f1ee5d97fc
Selva Subbiah A <125681297+Selvasubbiah-A@users.noreply.github.com>
2026-02-02 20:35:58 +0530

    Added conditional statement file
    Added conditional statement file

---
+++ Condition_statements.py
@@ -0,0 +1,97 @@
+# print("conditional statements")
+
+
+# a = 3
+# if a > 0:
+#     print('A is a positive number')
+
+
+# n = 2
+
+# if n < a:
+#     print("a ig bigger than n")
+
+
+# a = 0
+# if a < 0:
+#     print('A is a negative number')
+# else:
+#     print('A is a positive number')
+
+
+# a = 0
+# if a > 0:
+#     print('A is a positive number')
+# elif a < 0:
+#     print('A is a negative number')
+# elif a > 0 :
+#     print("")
+# else:
+#     print('A is zero')
+
+
+# a = -3
+# print('A is positive') if a > 0 else print('A is negative') 
+
+
+# a = 8647648
+# if a > 0:
+#     if a % 2 == 0:
+#         print('A is a positive and even integer')
+#     else:
+#         print('A is a positive and odd number')
+# elif a == 0:
+#     print('A is zero')
+# else:
+#     print('A is a negative number')
+
+
+# a = 57
+# if a > 0 and a % 2 == 0:
+#     print('A is an even and positive integer')
+# elif a > 0 and a % 2 !=  0:
+#     print('A is a positive integer')
+# elif a == 0:
+#     print('A is zero')
+# else:
+#     print('A is negative')
+
+
+
+# user = 'admin'
+# access_level = 2
+# if user == 'admin' or access_level >= 4:
+#     print('Access granted!')
+# else:
+#     print('Access denied!')
+
+
+
+"""
+You need to declare a list variable
+Need to get the limit of values from user
+Get the list of elements from user in output
+check if the value has duplicates
+print the result as it is 
+finally print the result list with distict records sorted in desc order 
+"""
+
+# lst = []
+
+# limit = int(input("Enter the length of your list : "))
+
+# for i in range(limit):
+
+#     element = int(input(f"Enter your list element for {i} index : "))
+
+#     lst.append(element)
+
+# print(f"Your entered list is : {lst}")
+
+# st = set(lst)
+
+# lst = list(st)
+
+# lst.sort(reverse=True)
+
+# print(f"Your list after removing duplicates and ordered : {lst}")
