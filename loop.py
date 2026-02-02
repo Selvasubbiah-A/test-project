d612392dd7ecac6a9c7a22fbf580dc6c35f96455
Selva Subbiah A <125681297+Selvasubbiah-A@users.noreply.github.com>
2026-02-02 20:29:29 +0530

    added loop file into Repo
    added loop file into Repo

---
+++ loop.py
@@ -0,0 +1,138 @@
+# print("looping statements")
+
+# numbers = [0, 1, 2, 3, 4, 5]
+# for number in numbers: 
+#     print(number)       
+
+
+# language = 'Python'
+# # for letter in language:
+# #     print(letter)
+
+
+# for i in range(len(language)):
+#     print(language[i])
+
+
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count = count + 1
+
+
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count = count + 1
+#     if count == 3:
+#         break
+
+
+# count = 0
+# while count < 5:
+#     if count == 3:
+#         count = count + 1
+#         continue
+#     print(count)
+#     count = count + 1
+
+
+# language = 'Python'
+# for letter in language:
+#     print(letter)
+
+
+# for i in range(len(language)):
+#     print(language[i])
+
+
+# numbers = (0, 1, 2, 3, 4, 5)
+# for number in numbers:
+#     print(number)
+
+
+# person = {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'age':250,
+#     'country':'Finland',
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#     }
+# }
+# # for key in person:
+# #     print(key)
+
+# for key, value in person.items():
+#     print(key, ":", value) # this way we get both keys and values printed out
+
+
+
+# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+# for company in it_companies:
+#     print(company)
+
+
+# numbers = (0,1,2,3,4,5)
+# for number in numbers:
+    
+#     if number == 3:
+#         continue
+#     print(number)
+    
+
+# for i in range(5, 11):
+#     print(i)
+
+
+# person = {
+#     'first_name': 'Asabeneh',
+#     'last_name': 'Yetayeh',
+#     'age': 250,
+#     'country': 'Finland',
+#     'is_marred': True,
+#     'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address': {
+#         'street': 'Space street',
+#         'zipcode': '02210'
+#     }
+# }
+# for key in person:
+#     if key == 'skills':
+#         for skill in person['skills']:
+#             print(skill)
+
+
+# for number in range(11):
+#     print(number)   # prints 0 to 10, not including 11
+# else:
+#     print('The loop stops at', number)
+
+
+"""
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+"""
+
+# n = int(input("Enter the height for your triangle : "))
+
+# for i in range(n, 0, -1):
+#     print(" " * (n - i)+ "* "*i)
+
+
+# language = 'Python'
+
+
+# for i in range(len(language)-1, -1, -1):
+#     print(language[i])
+
+
+
